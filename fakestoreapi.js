d4ebc6b4496e65770193297a9c2d3f38fa317e71
odran21 <jleonardoguevaram@gmail.com>
2025-10-07 11:10:56 -0500

    obteniendo todos los productos de FakeStoreAPI

--- fakestoreapi.js
+++ fakestoreapi.js
@@ -1,1 +1,15 @@
-// Aqui vamos a crear las funciones que vamos a exportar con node modules para usar desde el index.js
+// Aqui las funciones que vamos a exportar con node modules para usar desde el index.js
+const url = "https://fakestoreapi.com/products"
+async function getAllProducts() {
+    try {
+        const response = await fetch(url, {method: "GET"})
+        if(!response.ok){ throw new Error(`HTTP status: ${response.status}`) }
+        const data = await response.json()
+        console.log(data)
+    } catch (error) {
+        console.error(`${error.message}`)
+    } finally {
+        console.log(`Final`)
+    }
+}
+export {getAllProducts}
--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 // Aqui vamos a importar las funciones desde ./fakestoreapi.js
-console.log(`Hola con Node.js`)
+
+import { getAllProducts } from "./fakestoreapi.js"
+
+const [,, comando] = process.argv
+
+async function main(){
+    switch (comando.toLowerCase()) {
+        case "getall":
+            // Hay que ingresar por consola -> npm start getall para mostrar todos los productos
+            console.log(`Obteniendo todos los productos...\n `)
+            await getAllProducts()
+            break;
+    
+        default:
+            console.error(`Comando desconocido: "${comando}"`)
+            break;
+    }
+}
+main().catch(error => {console.error(`Error en el programa: ${error.message}`)})
