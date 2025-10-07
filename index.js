0ec92f4bc936c089eb57aae2634b99ee0effd284
odran21 <jleonardoguevaram@gmail.com>
2025-10-07 11:20:10 -0500

    asignacion del comando a minusculas en una variable

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 // Aqui vamos a importar las funciones desde ./fakestoreapi.js
-
 import { getAllProducts } from "./fakestoreapi.js"
 
 const [,, comando] = process.argv
-
+const comandoMinusculas = comando.toLowerCase();
 async function main(){
-    switch (comando.toLowerCase()) {
+    switch (comandoMinusculas) {
         case "getall":
             // Hay que ingresar por consola -> npm start getall para mostrar todos los productos
-            console.log(`Obteniendo todos los productos...\n `)
+            console.log(`Obteniendo todos los productos...\n por favor espere un momento`)
             await getAllProducts()
             break;
     
